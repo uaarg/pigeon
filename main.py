13d364b4fa244e55bb61ab682bf7d0a89a314d4b
Cindy Xiao <c.xiao@live.com>
2014-03-01 16:56:25 -0700

    Open specific images, added cursor.

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ Main interface for ground station.
 
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QImage, QCursor, QPixmap
 # Import all from QtWidgets for development for now.
+# So for classes that are part of QtWidgets, I've specifically stated QtWidget.Class() for now.
 from PyQt5.QtWidgets import *
 
 
 class GroundStation(QtWidgets.QMainWindow):
-    def __init__(self):
+    def __init__(self, sysargs):
         super(GroundStation, self).__init__()
 
         # Set up window
@@ -20,32 +22,48 @@ class GroundStation(QtWidgets.QMainWindow):
         self.setGeometry(300, 300, 250, 150)
 
         # Set up main display area
-        self.imageLabel = QLabel()
+        self.imageLabel = QtWidgets.QLabel()
         self.setCentralWidget(self.imageLabel)
 
         # Set up cursor
-        self.cursor = QCursor()
+        self.cursor = QCursor(Qt.CrossCursor)
         self.setCursor(self.cursor)
+        self.getCursorPosition()
 
-        self.openImage("data/processed/101.jpg")
-        self.show()
+        # Open image
+        try:
+            self.imagePath = sysargs[1]
+        except:
+            print("Usage: python main.py [image_path]")
+            sys.exit()
+        else:
+            self.openImage(self.imagePath)
+
 
     def openImage(self, filename):
         """
         Opens image from specified path.
         """
         if filename:
-            image = QtGui.QImage(filename)
+            image = QImage(filename)
             if image.isNull():
                 QMessageBox.information(self, "Error", "Can't load image %s." %(filename))
             else:
                 # Convert from QImage to QPixmap, and display
                 self.imageLabel.setPixmap(QPixmap.fromImage(image))
 
+    def getCursorPosition(self):
+        """
+        Gets the position of the mouse cursor on the image.
+        """
+        self.cursorPosition = QWidget.mapFromGlobal(self, self.cursor.pos())
+
+
 def main():
     print("Ground station running.")
     app = QtWidgets.QApplication(sys.argv)
-    gs = GroundStation()
+    gs = GroundStation(sys.argv)
+    gs.show()
     sys.exit(app.exec_())
 
 if __name__ == "__main__":
