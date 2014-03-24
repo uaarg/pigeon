7e1523bcd6849ede4446bcc346bc4e9eea1c9595
Emmanuel Odeke <odeke@ualberta.ca>
2014-03-23 19:04:02 -0600

    Utility for connecting to db

---
+++ DbLiason.py
@@ -0,0 +1,89 @@
+#!/usr/bin/python3
+
+import os
+import json
+import collections
+import urllib.request, urllib
+
+class DbConn:
+  def __init__(self, baseUrl):
+    self.baseUrl = baseUrl
+
+  def __urlRequest(self, method, **getData):
+    fmtdData = json.dumps(getData)
+    req = urllib.request.Request(self.baseUrl)
+    req.add_header('Content-Type', 'application/json')
+    req.get_method = lambda : method.upper()
+    dataOut = dict()
+    try:
+      uR = urllib.request.urlopen(req, bytes(fmtdData, encoding='utf-8'))
+    except Exception as e:
+      print(e)
+      dataOut['reason'] = e
+    else:
+      dataOut['value'] = uR.read()
+    return dataOut
+
+  def get(self, data):
+    return self.__urlRequest('get', **data)
+
+  def put(self, data):
+    return self.__urlRequest('put', **data)
+
+  def post(self, data):
+    return self.__urlRequest('post', **data)
+
+  def delete(self, data):
+    return self.__urlRequest('delete', **data)
+
+class HandlerLiason(object):
+  def __init__(self, baseUrl, *args, **kwargs):
+    self.baseUrl = baseUrl
+    self.__callableCache = collections.defaultdict(
+      lambda *arg, **kwargs : 'Not yet defined: {a}, {k}'.format(a=args, k=kwargs)
+    )
+    self.handler = DbConn(baseUrl)
+
+  def postConn(self, data):
+    return self.handler.post(data)
+
+  def deleteConn(self, data):
+    return self.handler.delete(data)
+
+  def putConn(self, data):
+    return self.handler.put(data)
+
+  def getConn(self, data):
+    return self.handler.get(data)
+
+class GCSHandler(object):
+  def __init__(self, baseUrl, *args, **kwargs):
+    self.baseUrl = baseUrl
+    self.__imageHandler = HandlerLiason(baseUrl + '/imageHandler')
+    self.__markerHandler = HandlerLiason(baseUrl + '/markerHandler')
+
+  @property
+  def imageHandler(self): return self.__imageHandler
+
+  @property
+  def markerHandler(self): return self.__markerHandler
+
+def main():
+  gcsH = GCSHandler('http://192.168.1.102:8001/gcs') 
+  imageHandler  = gcsH.imageHandler
+  markerHandler = gcsH.markerHandler
+  with open(__file__, 'r') as f:
+    blob = f.read()
+    markerData = dict(
+      author=os.environ['USER'], title='Red SUV',
+      x=100.1, y=24.1, associatedImage_id=1
+    )
+    print(markerHandler.postConn(markerData))
+
+    print(
+      imageHandler.postConn(
+        dict(uri=__file__,author="Emmanuel Odeke", title='AirField', blob=blob)
+      )
+    )
+if __name__ == '__main__':
+  main()
