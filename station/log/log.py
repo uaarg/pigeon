7f10cf6078ee71164e9922cbe083377a85a23d5f
Cameron Lee <cam.w.lee@gmail.com>
2016-09-16 21:10:00 -0600

    Limited size of log files to 50 MB.

--- station/log/log.py
+++ station/log/log.py
@@ -1,4 +1,5 @@
 import logging
+import logging.handlers
 import os
 
 NONE = logging.CRITICAL + 1
@@ -10,12 +11,13 @@ DEBUG = logging.DEBUG
 
 def initialize(file_level=logging.DEBUG, console_level=logging.CRITICAL, filename="log"):
 
-    log_file = os.path.join(*["data", filename + os.extsep + "log"])
+    log_file = os.path.join(*["data", "logs", "main"])
 
     logger = logging.getLogger()
     logger.setLevel(logging.DEBUG)
 
-    file_handler = logging.FileHandler(log_file)
+    max_megabytes = 10
+    file_handler = logging.handlers.RotatingFileHandler(log_file, maxBytes=max_megabytes*1024*1024, backupCount=5)
     file_formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
     file_handler.setFormatter(file_formatter)
     file_handler.setLevel(file_level)
