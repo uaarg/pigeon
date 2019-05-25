59a7ef425107ce40d89f17bff4da976c683d289d
Ryan Sandoval <rsandova@ualberta.ca>
2019-05-25 16:30:45 -0400

    run script now adds client lib to PYTHONPATH

--- run.sh
+++ run.sh
@@ -8,4 +8,14 @@
 
 DIR=$(cd $(dirname $0) && pwd)
 
+if ! [ -d ${DIR}/modules/interop/client ]; then
+    echo "Could not find ${DIR}/modules/interop/client containing interop client library. Aborting."
+    exit 1
+fi
+
+CLIENT=${DIR}/modules/interop/client
+
+export PYTHONPATH=${PYTHONPATH}:$CLIENT
+
 cd station && ${DIR}/env/venv3/bin/python3 station.py
+exit 0
