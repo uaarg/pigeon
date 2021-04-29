ef82bb1e09c90391b4e0d64acd1622bb669a6074
Ryan <rsandova@ualberta.ca>
2021-04-28 20:08:13 -0600

    Re-added pqyt5 and shapely apt dependencies

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -53,6 +53,8 @@ fi
 # Install Pigeon specific apt-gets
 echo "Installing Apt Packages..."
 apt-get -y install \
+    qtdeclarative5-dev qtmultimedia5-dev python3-pyqt5 \
+    python3-shapely \
     libxml2-dev libxslt1-dev\
     libzbar0 \
     protobuf-compiler
