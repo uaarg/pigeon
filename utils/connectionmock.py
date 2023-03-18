cd9f7722f05d518aa6744bbf3c7e193145348ce8
Aidan Olsen <a.olsen2024@icloud.com>
2023-03-17 18:35:41 -0600

    Update the connectionmock script to periodically send heatbeats

--- utils/connectionmock.py
+++ utils/connectionmock.py
@@ -16,21 +16,31 @@ import time
 DEVICE = 'tcpin:localhost:1234'
 conn: mavutil.mavfile = mavutil.mavlink_connection(DEVICE)
 
+i = 0
 while True:
     msg = conn.recv_match()
-    if not msg:
-        time.sleep(0.01)
-        continue
 
-    print(msg)
+    if msg:
+        print(msg)
 
-    if msg.get_type() == 'HEARTBEAT':
-        print("\n\n*****Got message: %s*****" % msg.get_type())
-        print("Message: %s" % msg)
-        print("\nAs dictionary: %s" % msg.to_dict())
-        # Armed = MAV_STATE_STANDBY (4), Disarmed = MAV_STATE_ACTIVE (3)
-        print("\nSystem status: %s" % msg.system_status)
+        if msg.get_type() == 'HEARTBEAT':
+            print("\n\n*****Got message: %s*****" % msg.get_type())
+            print("Message: %s" % msg)
+            print("\nAs dictionary: %s" % msg.to_dict())
+            # Armed = MAV_STATE_STANDBY (4), Disarmed = MAV_STATE_ACTIVE (3)
+            print("\nSystem status: %s" % msg.system_status)
 
+            conn.mav.heartbeat_send(
+                type=mavutil.mavlink.MAV_TYPE_GENERIC,
+                autopilot=mavutil.mavlink.MAV_AUTOPILOT_INVALID,
+                base_mode=0,
+                custom_mode=0,
+                system_status=0)
+
+    i += 1
+    time.sleep(0.01)
+
+    if i % 1000:
         conn.mav.heartbeat_send(
             type=mavutil.mavlink.MAV_TYPE_GENERIC,
             autopilot=mavutil.mavlink.MAV_AUTOPILOT_INVALID,
