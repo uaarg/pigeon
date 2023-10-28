268e013cc84a8a6cd3836a9f98a1e7ed0fa9625f
Kyle Chang <59430996+kylechang133@users.noreply.github.com>
2023-10-28 15:03:39 -0600

    wip

--- tools/mock_uav.py
+++ tools/mock_uav.py
@@ -5,6 +5,7 @@ import time
 import queue
 
 from pymavlink import mavutil
+from pymavlink.dialects.v20 import common as mavlink2
 
 from pigeon.comms.services.common import HearbeatService, StatusEchoService, Command
 
@@ -13,6 +14,33 @@ def disconnect():
     print("Error! Disconnected from server. Exiting", file=sys.stderr)
     sys.exit(1)
 
+def send_image(conn):
+    """Sends a test image to the GUI"""
+    with open("tools/test_image.JPG", "rb") as f:
+        image_data = f.read()
+    
+    # Define the MAVLink payload size for ENCAPSULATED_DATA (253 bytes)
+    chunk_size = 253
+    total_packets = (len(image_data) + chunk_size - 1) // chunk_size
+
+    # Send each chunk in an ENCAPSULATED_DATA message
+    for i in range(total_packets):
+        chunk = image_data[i * chunk_size: (i+1) * chunk_size]
+        msg = mavlink2.MAVLink_encapsulated_data_message(i, chunk)
+        conn.write(msg)
+
+    # Send the DATA_TRANSMISSION_HANDSHAKE message
+    handshake_msg = mavlink2.MAVLink_data_transmission_handshake_message(
+        mavlink2.MAVLINK_DATA_STREAM_IMG_JPEG,  # type of data stream, use JPEG
+        #len(image_data),  # total size of the image
+        0,  # width (0 for unknown)
+        0,  # height (0 for unknown)
+        0,  # packets (0 for unknown)
+        0,  # payload size per packet (0 for unknown)
+        total_packets,  # total packets
+        0,  # jpg quality (0 for unknown)
+    )
+    conn.write(handshake_msg)
 
 def main(device: str):
     # Uses a similar struture to pigeon.comms.uav
@@ -31,6 +59,8 @@ def main(device: str):
     ]
 
     commands.put(Command.statustext("Started UAV Mocker (from %s)" % device))
+
+    send_image(conn)
 
     while connected:
         for service in services:
